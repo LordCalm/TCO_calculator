1ecff490f9adf4f8609fe31c7d7a07822e7a13ad
Dmitriy <43829114+LordCalm@users.noreply.github.com>
2023-03-01 14:33:12 +0400

    Добавлен компонент для ввода данных

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,43 @@
-<template>
-  <div class="container">
-      <div class="container">
-        <input type="number" v-bind:value="number1" @input="inputNumber1" id="number1">
-      </div>
-      <div class="container">
-        <input type="number" v-bind:value="number2" @input="inputNumber2" id="number2">
-      </div>
-      <div class="container">
-        <div id="sum">
-          <p>Сумма: {{ sum }}</p>
-        </div>
-      </div>
-    </div>
-</template>
-
 <script>
+import CalculationParameter from './components/CalculationParameter.vue';
 export default {
+  components: {
+    CalculationParameter
+  },
   data() {
     return {
-      number1: 0,
-      number2: 0,
-    }
-  },
-  methods: {
-    inputNumber1(event) {
-      this.number1 = event.target.value;
-    },
-    inputNumber2(event) {
-      this.number2 = event.target.value;
-    }
-  },
-  computed: {
-    // геттер вычисляемого свойства
-    sum() {
-      return Number(this.number1) + Number(this.number2);
+      parameterList: [
+        {
+          parameterName: 'Оклад программиста',
+          number: 0
+        }
+      ],
+      // parameter: {
+      //     parameterName: '',
+      //     number: 0
+      // }
     }
   }
+  // computed: {
+  //   sum() {
+  //     return Number(this.number1) + Number(this.number2);
+  //   }
+  // }
 }
 </script>
+
+<template>
+  <div class="container-fluid">
+    <div class="container">
+      Hello!
+      <CalculationParameter
+      v-model="parameterList[0].number" /> 
+      <!-- title = "Hello!" -->
+      <!-- <div id="sum">
+        <p>Сумма: {{ sum }}</p>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+
